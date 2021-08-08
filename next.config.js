7785f42ab1b28e62483545f68001c7f7c237f93f
Luis <luis.cruz@urvenue.com>
2021-08-08 00:45:16 -0700

    Mapbox

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
         domains: [
             'links.papareact.com'
         ],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoibHVjaG9ybTIzIiwiYSI6ImNqbGY2c203cTB0cGIzd2xxajkxajBucDMifQ.SFyXq7VvS4-LMSFcY0hnlw'
     }
 }
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,11 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
+import MiniInfoCard from "../components/MiniInfoCard";
+import ReactMapGL, { Marker, Popup, FlyToInterpolator } from 'react-map-gl';
+import { useState, useCallback } from "react";
+import getCenter from 'geolib/es/getCenter';
+import Image from 'next/image';
 
 function Search({ searchResults }) {
     const router = useRouter();
@@ -14,13 +19,35 @@ function Search({ searchResults }) {
 
     const range = `${formattedStartDate} to ${formattedEndDate}`;
 
+    const [selectedLocation, setSelectedLocation] = useState({});
+
+    // Transformation
+    const coordinates = searchResults.map(result => ({
+        longitude: result.long,
+        latitude: result.lat,
+    }));
+
+    const center = getCenter(coordinates);
+
+
+    const [viewport, setViewport] = useState({
+        width: '100%',
+        height: '100%',
+        latitude: center.latitude,
+        longitude: center.longitude,
+        zoom: 11,
+        transitionInterpolator: new FlyToInterpolator({ speed: 1.2 }),
+        transitionDuration: 'auto'
+    }); 
+
     return (
         <div>
             <Header isTransparent={false} placeholder={`${location} | ${range} | ${noOfGuests}`} />
-            <main className="flex">
+            <main className="flex overflow-y-scroll">
                 <section className="flex-grow pt-14 px-6">
                     <p className="text-xs">300+ stays from {range} - for {noOfGuests} guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
+                    <p className="text-xs mb-6">Click a destination to navigate inside the map.</p>
 
                     <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
                         <p className="button">Cancellation Flexibility</p>
@@ -38,21 +65,93 @@ function Search({ searchResults }) {
                             description,
                             star,
                             price,
-                            total
+                            total,
+                            long,
+                            lat,
                         }) => (
-                            <InfoCard
-                                img={img}
-                                location={location}
-                                title={title}
-                                description={description}
-                                star={star}
-                                price={price}
-                                total={total}
-                            />
+                            <div key={title} onClick={() => {
+                                setViewport({
+                                    longitude: long,
+                                    latitude: lat,
+                                    zoom: 11,
+                                    transitionInterpolator: new FlyToInterpolator(),
+                                    transitionDuration: 'auto'
+                                });
+                            }}>
+                                <InfoCard
+
+                                    img={img}
+                                    location={location}
+                                    title={title}
+                                    description={description}
+                                    star={star}
+                                    price={price}
+                                    total={total}
+                                />
+                            </div>
                         ))}
                     </div>
                 </section>
+                <section className="hidden xl:inline-flex xl:min-w-[600px] overflow-hidden">
+                    {/* <Map searchResults={searchResults}/> */}
+                    <ReactMapGL
+                        mapStyle='mapbox://styles/luchorm23/cks2fsog64b5h18mlksef3n44'
+                        mapboxApiAccessToken={process.env.mapbox_key}
+                        onViewportChange={(newviewport) => setViewport(newviewport)}
+
+                        {...viewport}
+                    >
+                        {searchResults.map(result => (
+                            <div key={result.long}>
+                                <Marker                                   
+                                    longitude={result.long}
+                                    latitude={result.lat}
+                                    // offsetLeft={-20}
+                                    // offsetTop={-10}
+                                >
+
+                                    <Image
+                                        onClick={() => setSelectedLocation(result)}
+                                        className="cursor-pointer"
+                                        width={40}
+                                        height={40}
+                                        src="/pin.svg"
+                                        aria-label="push-pin"
+                                        role='img'
+                                    />
+
+                                </Marker>
+                                {/* The popup that should show if we click on a Marker */}
+                                {selectedLocation.long === result.long && (
+                                    <Popup
+                                        closeOnClick={true}
+                                        onClose={() => {
+                                            setSelectedLocation({});                                            
+                                        }}
+                                    
+                                        latitude={result.lat}
+                                        longitude={result.long}
+                                        
+                                        // offsetLeft={90}
+                                    >
+                                         
+                                             <MiniInfoCard
+                                                img={result.img}
+                                                location={result.location}
+                                                title={result.title}
+                                                description={result.description}
+                                                star={result.star}
+                                                price={result.price}
+                                                total={result.total}
+                                                />                                         
+                                    </Popup>
+                                )}
+                            </div>
+                        ))}
+                    </ReactMapGL>
+                </section>
             </main>
+
             <Footer />
         </div>
     )
